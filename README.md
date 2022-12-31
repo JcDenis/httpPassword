728434d4909fea91289f0ebc051f1459bee5f445
Jean-Christian Denis <contact@jcdenis.fr>
2022-12-31 01:49:10 +0100

    release 1.0

---
+++ README.md
@@ -0,0 +1,45 @@
+# README
+
+[![Release](https://img.shields.io/github/v/release/JcDenis/httpPassword)](https://github.com/JcDenis/httpPassword/releases)
+[![Date](https://img.shields.io/github/release-date/JcDenis/httpPassword)](https://github.com/JcDenis/httpPassword/releases)
+[![Issues](https://img.shields.io/github/issues/JcDenis/httpPassword)](https://github.com/JcDenis/httpPassword/issues)
+[![Dotclear](https://img.shields.io/badge/dotclear-v2.24-blue.svg)](https://fr.dotclear.org/download)
+[![Dotaddict](https://img.shields.io/badge/dotaddict-official-green.svg)](https://plugins.dotaddict.org/dc2/details/httpPassword)
+[![License](https://img.shields.io/github/license/JcDenis/httpPassword)](https://github.com/JcDenis/httpPassword/blob/master/LICENSE)
+
+## WHAT IS HTTPPASSWORD ?
+
+_httpPassword_ is a plugin for the open-source 
+web publishing software called Dotclear.
+
+Its helps to manage .httppassword files to make a blog private.
+
+## REQUIREMENTS
+
+ _httpPassword_ requires: 
+
+  * httpPassword permission to configure
+  * Dotclear 2.24
+  * Write permissions on blogs directories
+  * A web server that support .htpassword files. (Apache)
+
+## USAGE
+
+First install _httpPassword_, manualy from a zip package or from 
+Dotaddict repository. (See Dotclear's documentation to know how do this)
+
+...
+
+## LINKS
+
+ * License : [GNU GPL v2](https://www.gnu.org/licenses/old-licenses/lgpl-2.0.html)
+ * Source & contribution : [GitHub Page](https://github.com/JcDenis/kUtRL)
+ * Packages & details:  [Dotaddict Page](https://plugins.dotaddict.org/dc2/details/kUtRL)
+ * Discussion & Help: [Dotclear Forum](http://forum.dotclear.org/viewtopic.php?pid=331158)
+
+## CONTRIBUTORS
+
+ * Frederic PLE (author)
+ * Jean-Christian Denis
+
+ You are welcome to contribute to this code.
---
+++ locales/fr/main.lang.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @package Dotclear
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+l10n::$locales['Manage http password blog protection']                                     = 'Gérer la protection du blog par mot de passe HTTP';
+l10n::$locales['No encryption']                                                            = 'pas de cryptage';
+l10n::$locales['Logins history']                                                           = 'Historique des logins';
+l10n::$locales['Authorized users']                                                         = 'Utilisateurs autorisés';
+l10n::$locales['No write permissions on blogs directories.']                               = 'Aucun droit d\'écriture sur le répertoire du blog.';
+l10n::$locales['Settings successfully updated.']                                           = 'Paramètres mis à jour.';
+l10n::$locales['Logs successfully cleared.']                                               = 'Logs effacé.';
+l10n::$locales['Logins successfully updated.']                                             = 'Logins mis à jour.';
+l10n::$locales['Select section:']                                                          = 'Sélectionner une section :';
+l10n::$locales['Enable http password protection on this blog']                             = 'Activer la protection du blog par mot de passe http';
+l10n::$locales['Crypt algorithm:']                                                         = 'Algorithme de cryptage :';
+l10n::$locales['Some web servers does not surpport plaintext (no) encryption.']            = 'Certains serveurs web ne supportent pas le cryptage "plaintext".';
+l10n::$locales['If you change crypt algo, you must edit and resave each users passwords.'] = 'Si vous changer l\'algorithme de cryptage, vous devrez modifier et sauver tous les mots de passes.';
+l10n::$locales['Authentication message:']                                                  = 'Message d\'authentification :';
+l10n::$locales['Logins history is empty.']                                                 = 'L\'historique des logins est vide.';
+l10n::$locales['Clear logs']                                                               = 'Effacer les logs';
+l10n::$locales['List of %s last logins.']                                                  = 'Liste des %s derniers logins.';
+l10n::$locales['Authorized users list is empty.']                                          = 'La listes des utilisateurs autorisés est vide.';
+l10n::$locales['List of %s authorized users.']                                             = 'Liste des %s utilisateurs autorisés.';
+l10n::$locales['New password']                                                             = 'Nouveau mot de passe';
+l10n::$locales['Change password']                                                          = 'Modifier le mot de passe';
+l10n::$locales['Add a user']                                                               = 'Ajouter un utilisateur';
+l10n::$locales['Login:']                                                                   = 'Identifiant :';
+l10n::$locales['Manage .htpasswd file to make the blog private']                           = 'Gestion du fichier .htpasswd pour rendre le blog privé.';
